6f7ca37b6ba7de5c1a7866474f637b971161dad4
AloisBlue <aloismburu@gmail.com>
2019-02-06 11:37:57 +0300

    [#163720394] admin can create an office

---
+++ app/my_api/v1/offices.py
@@ -0,0 +1,37 @@
+# app/my_api/v1/offices.py
+
+from flask_restful import Resource, reqparse
+
+offices_list = [
+
+]
+
+class CreateOffice(Resource):
+    """docstring for CreateOffice."""
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'name',
+        type=str,
+        required=True,
+        help="Name of the political office missing"
+    )
+    parser.add_argument(
+        'type',
+        type=str,
+        required=True,
+        help="Type of the office not stated"
+    )
+
+    def post(self):
+        data = CreateOffice.parser.parse_args()
+        office_exist = [office for office in offices_list if office['name'] == data['name']]
+        if (len(office_exist) != 0):
+            return {'Message': 'The office is already registered in our system'}, 400
+        _id = len(offices_list) + 1
+        new_office = {
+            'id': _id,
+            'name': data['name'],
+            'type': data['type']
+            }
+        offices_list.append(new_office)
+        return {'Message': 'Office registered in the system!!!', 'Office': new_office}, 201
