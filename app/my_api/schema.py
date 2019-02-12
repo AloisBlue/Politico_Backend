306103d65fad2745138776d7276eae8f928a3ea7
AloisBlue <aloismburu@gmail.com>
2019-02-12 19:59:57 +0300

    [#163905558] setup database

---
+++ app/my_api/schema.py
@@ -0,0 +1,12 @@
+userstable = """CREATE TABLE if not exists Users(
+user_id SERIAL PRIMARY KEY,
+firstname varchar (50) NOT NULL,
+lastname varchar (50) NOT NULL,
+othername varchar (50) NOT NULL,
+email varchar(40) NOT NULL UNIQUE,
+phoneNumber varchar (20) NOT NULL,
+passportUrl varchar (255) NOT NULL,
+password_hash varchar(64) NOT NULL,
+isAdmin boolean DEFAULT 'f'
+);"""
+queries = [userstable]
