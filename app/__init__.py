2c2ce8a29d33180f753667a63ec6a4fed52bff14
AloisBlue <aloismburu@gmail.com>
2019-02-14 22:37:28 +0300

    [#163906180] save vote in the db

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from config import config
 from .my_api.v1.parties import CreateParty, GetAllParties, PartyById
 from .my_api.v1.offices import CreateOffice, GetAllOffices, OfficeById
 from .my_api.v2.auth import RegisterUser, LoginUser
-from .my_api.v2.vote import RegisterCandidate
+from .my_api.v2.vote import RegisterCandidate, CastVote
 from.my_api.database import database_init
 
 v1 = Blueprint('api', __name__)
@@ -48,3 +48,4 @@ api.add_resource(OfficeById, '/offices/<int:office_id>')
 api_v2.add_resource(RegisterUser, '/auth/signup')
 api_v2.add_resource(LoginUser, '/auth/login')
 api_v2.add_resource(RegisterCandidate, '/vote/candidate')
+api_v2.add_resource(CastVote, '/vote')
--- app/my_api/v2/vote.py
+++ app/my_api/v2/vote.py
@@ -2,6 +2,7 @@
 # imports
 from flask_restful import Resource, reqparse
 import psycopg2
+import datetime
 
 # local imports
 from ..database import database
@@ -39,6 +40,16 @@ class RegisterCandidate(Resource):
     )
 
     def post(self):
+        cur.execute(
+            """CREATE TABLE if not EXISTS Votes(
+            vote_id serial PRIMARY KEY,
+            user_id INT,
+            cast_date TIMESTAMP,
+            president varchar (50) NOT NULL,
+            governor varchar (50) NOT NULL,
+            mca varchar (50) NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES Users(user_id));"""
+        )
         data = RegisterCandidate.parser.parse_args()
         firstname = data['firstname']
         lastname = data['lastname']
@@ -67,6 +78,58 @@ class RegisterCandidate(Resource):
             })
             connection.commit()
             return {'Message': 'Candidate {} registered to vie for office of the {}.'.format(firstname, office), 'Details': data}, 201
+        except (Exception, psycopg2.DatabaseError) as error:
+            cur.execute("rollback;")
+            print(error)
+            return {'Message': 'current transaction is aborted'}, 500
+
+
+class CastVote(Resource):
+    """docstring for CastVote."""
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'president',
+        type=str,
+        required=True,
+        help="President not selected"
+    )
+    parser.add_argument(
+        'governor',
+        type=str,
+        required=True,
+        help="Governor not selected"
+    )
+    parser.add_argument(
+        'mca',
+        type=str,
+        required=True,
+        help="Mca not selected"
+    )
+
+    def post(self):
+        data = CastVote.parser.parse_args()
+        president = data['president']
+        governor = data['governor']
+        mca = data['mca']
+        # validations
+        while True:
+            if not president:
+                return {'Message': 'President option empty'}, 400
+            elif not governor:
+                return {'Message': 'Governor option empty'}, 400
+            elif not mca:
+                return {'Message': 'Mca option empty'}, 400
+            else:
+                break
+        try:
+            # sample user_id
+            user_id = 1
+            cast_date = datetime.datetime.utcnow()
+            cur.execute("INSERT INTO Votes(user_id, cast_date, president, governor, mca) VALUES(%(user_id)s, %(cast_date)s, %(president)s, %(governor)s, %(mca)s);", {
+                'user_id': user_id, 'cast_date': cast_date, 'president': president, 'governor': governor, 'mca': mca
+            })
+            connection.commit()
+            return {'Message': 'Vote casted!!!'}, 200
         except (Exception, psycopg2.DatabaseError) as error:
             cur.execute("rollback;")
             print(error)
