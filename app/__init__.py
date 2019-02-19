c1fd0dfa8d7a6b0790d40d86dd148e7f73802fd9
AloisBlue <aloismburu@gmail.com>
2019-02-19 21:03:50 +0300

    [#164083142] get all parties

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from .my_api.v1.parties import CreateParty, GetAllParties, PartyById
 from .my_api.v1.offices import CreateOffice, GetAllOffices, OfficeById
 from .my_api.v2.auth import RegisterUser, LoginUser, ResetPassword
 from .my_api.v2.vote import RegisterCandidate, CastVote, GetVotes, FilePetition
-from .my_api.v2.party import CreatePartyV2
+from .my_api.v2.party import CreatePartyV2, GetPartiesV2
 from.my_api.database import database_init
 
 v1 = Blueprint('api', __name__)
@@ -64,3 +64,4 @@ api_v2.add_resource(GetVotes, '/vote/<int:office_serial>')
 api_v2.add_resource(FilePetition, '/vote/petition')
 api_v2.add_resource(ResetPassword, '/auth/resetpassword')
 api_v2.add_resource(CreatePartyV2, '/parties')
+api_v2.add_resource(GetPartiesV2, '/parties')
