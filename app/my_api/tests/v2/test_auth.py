6016f32de332b831935de0d5069638c88d8b1535
AloisBlue <aloismburu@gmail.com>
2019-02-19 08:51:14 +0300

    message

--- app/my_api/tests/v2/test_auth.py
+++ app/my_api/tests/v2/test_auth.py
@@ -24,120 +24,130 @@ class TestAuth(unittest.TestCase):
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "https://miro.medium.com/max/2400/1*hiAQNjsT30LuqlZRmpdJkQ.jpeg",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
+        }
+        self.invalid_url = {
+            "email": "aloismburu@gmail.com",
+            "firstname": "Alois",
+            "lastname": "Blue",
+            "othername": "Success",
+            "phonenumber": "0778082345",
+            "passporturl": "flfd.com",
+            "password": "password",
+            "passwordconfirm": "password"
         }
         self.sign_bad_email_format = {
             "email": "aloismburu.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.password_less = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "pasord",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.password_match = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "pagaword"
+            "passwordconfirm": "pagaword"
         }
         self.empty_email = {
             "email": "",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_firstname = {
             "email": "aloismburu@gmail.com",
             "firstname": "",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_lastname = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_othername = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_phonenumber = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_passport_url = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "",
+            "phonenumber": "0778082345",
+            "passporturl": "",
             "password": "password",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_password = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "",
-            "passwordConfirm": "password"
+            "passwordconfirm": "password"
         }
         self.empty_password_confirm = {
             "email": "aloismburu@gmail.com",
             "firstname": "Alois",
             "lastname": "Blue",
             "othername": "Success",
-            "phoneNumber": "0778082345",
-            "passportUrl": "jdkjfld.com",
+            "phonenumber": "0778082345",
+            "passporturl": "jdkjfld.com",
             "password": "password",
-            "passwordConfirm": ""
+            "passwordconfirm": ""
         }
 
         # login variables
@@ -174,6 +184,14 @@ class TestAuth(unittest.TestCase):
         result = json.loads(response.data.decode())
         self.assertEqual(result['Message'], "Account for Alois was succesfully created!!!")
         self.assertEqual(201, response.status_code)
+
+    def test_invalid_url(self):
+        response = self.Client.post('/api/v2/auth/signup',
+                                    data=json.dumps(self.invalid_url),
+                                    content_type='application/json')
+        result = json.loads(response.data.decode())
+        self.assertEqual(result['Message'], "The passport URL is invalid")
+        self.assertEqual(400, response.status_code)
 
     def test_user_exists(self):
         resp = self.Client.post('/api/v2/auth/signup',
@@ -277,8 +295,8 @@ class TestAuth(unittest.TestCase):
     # login tests
     def test_login(self):
         resp = self.Client.post('/api/v2/auth/signup',
-                                    data=json.dumps(self.user_signs),
-                                    content_type='application/json')
+                                data=json.dumps(self.user_signs),
+                                content_type='application/json')
         response = self.Client.post('/api/v2/auth/login',
                                     data=json.dumps(self.login),
                                     content_type='application/json')
@@ -304,8 +322,8 @@ class TestAuth(unittest.TestCase):
 
     def test_login_invalid_credentials(self):
         resp = self.Client.post('/api/v2/auth/signup',
-                                    data=json.dumps(self.user_signs),
-                                    content_type='application/json')
+                                data=json.dumps(self.user_signs),
+                                content_type='application/json')
         response = self.Client.post('/api/v2/auth/login',
                                     data=json.dumps(self.invalid_credentials),
                                     content_type='application/json')
@@ -326,14 +344,12 @@ class TestAuth(unittest.TestCase):
                                     data=json.dumps(self.login_empty_password),
                                     content_type='application/json')
         result = json.loads(response.data.decode())
-        self.assertEqual(result['Message'], "You must provide a password")
+        self.assertEqual(result['Message'], "You must provide a password ")
         self.assertEqual(400, response.status_code)
 
     def tearDown(self):
-        schema_candidate = """DROP TABLE if exists "candidates" CASCADE;"""
         schema_user = """DROP TABLE if exists "users" CASCADE;"""
-        schema_vote = """DROP TABLE if exists "votes" CASCADE;"""
-        schemas = [schema_candidate, schema_user, schema_vote]
+        schemas = [schema_user]
         for schema in schemas:
             if schema:
                 self.cur.execute(schema)
